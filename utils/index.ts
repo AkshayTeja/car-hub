f35e19b18977fb550609032beb13c94a29b5ea5a
Axay17 <akshayteja04@gmail.com>
2025-01-03 22:51:36 +0530

    Car Card added

---
+++ utils/index.ts
@@ -0,0 +1,28 @@
+export async function fetchCars(){
+    const headers = {
+            'x-rapidapi-key': '2863c7fa34mshb01e8154cb2410bp19d1dajsn636607c01120',
+            'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',{
+        headers: headers,
+    });
+
+    const result = await response.json();
+    return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of vehicle age
+  
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+    return rentalRatePerDay.toFixed(0);
+  };
